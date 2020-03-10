be566879dc204a5c9f5fe2df4fae37f734bc7b17
hughgreethead <hugh.greethead@gmail.com>
2020-03-11 10:08:51 +1100

    Updated and added printing helper methods

--- stock_price_check.rb
+++ stock_price_check.rb
@@ -81,14 +81,14 @@ end
 def get_price_change_52w_h(stock)
     stock_price = get_stock_price(stock)
     week_52_high = get_52w_high(stock)
-    return stock_price - week_52_high
+    return (stock_price - week_52_high).round(4)
 end
 
 # Get price change from 52 week low to current price
 def get_price_change_52w_l(stock)
     stock_price = get_stock_price(stock)
     week_52_low = get_52w_low(stock)
-    return stock_price - week_52_low
+    return (stock_price - week_52_low).round(4)
 end
 
 # Get percentage price change from 52 week high to current price
@@ -125,44 +125,99 @@ def print_single_stock_price_pct(stock)
     rows = []
     # Check if price is up from yesterday, if so colour the price and % change green
     # Price values are converted into strings for purposes of printing in order to colorize output
-    if get_price_change_y_day(stock) > 0
-        rows.push([get_stock_symbol(stock), "$".colorize(:green) + get_stock_price(stock).to_s.colorize(:green), 
+    if get_price_change_y_day(stock) > 0 && get_pct_change_ytd(stock) > 0
+        rows.push([get_stock_symbol(stock), "$".colorize(:green) + get_stock_price(stock).to_s.colorize(:green),
+                (get_pct_change_y_day(stock)*100).round(2).to_s.colorize(:green) + "%".colorize(:green),
+                (get_pct_change_ytd(stock)*100).round(2).to_s.colorize(:green) + "%".colorize(:green),
+                (get_pct_change_52w_h(stock)*100).round(2).to_s.colorize(:red)+ "%".colorize(:red), 
+                (get_pct_change_52w_l(stock)*100).round(2).to_s.colorize(:green) + "%".colorize(:green)])
+    elsif get_price_change_y_day(stock) > 0 && get_pct_change_ytd(stock) < 0
+        rows.push([get_stock_symbol(stock), "$".colorize(:green) + get_stock_price(stock).to_s.colorize(:green),
                 (get_pct_change_y_day(stock)*100).round(2).to_s.colorize(:green) + "%".colorize(:green), 
+                (get_pct_change_ytd(stock)*100).round(2).to_s.colorize(:red) + "%".colorize(:red),
                 (get_pct_change_52w_h(stock)*100).round(2).to_s.colorize(:red)+ "%".colorize(:red), 
                 (get_pct_change_52w_l(stock)*100).round(2).to_s.colorize(:green) + "%".colorize(:green)])
     # Check if price is down from yesterday, if so colour the price and % change red
-    elsif get_price_change_y_day(stock) < 0
+    elsif get_price_change_y_day(stock) < 0 && get_pct_change_ytd(stock) > 0
         rows.push([get_stock_symbol(stock), "$".colorize(:red) + get_stock_price(stock).to_s.colorize(:red), 
                 (get_pct_change_y_day(stock)*100).round(2).to_s.colorize(:red) + "%".colorize(:red),
+                (get_pct_change_ytd(stock)*100).round(2).to_s.colorize(:green) + "%".colorize(:green),
+                (get_pct_change_52w_h(stock)*100).round(2).to_s.colorize(:red) + "%".colorize(:red), 
+                (get_pct_change_52w_l(stock)*100).round(2).to_s.colorize(:green) + "%".colorize(:green)])               
+    elsif get_price_change_y_day(stock) < 0 && get_pct_change_ytd(stock) < 0
+        rows.push([get_stock_symbol(stock), "$".colorize(:red) + get_stock_price(stock).to_s.colorize(:red), 
+                (get_pct_change_y_day(stock)*100).round(2).to_s.colorize(:red) + "%".colorize(:red),
+                (get_pct_change_ytd(stock)*100).round(2).to_s.colorize(:red) + "%".colorize(:red),
                 (get_pct_change_52w_h(stock)*100).round(2).to_s.colorize(:red) + "%".colorize(:red), 
                 (get_pct_change_52w_l(stock)*100).round(2).to_s.colorize(:green) + "%".colorize(:green)])
     else
         rows.push([get_stock_symbol(stock), "$" + get_stock_price(stock).to_s, "0%", 
+                (get_pct_change_ytd(stock)*100).round(2).to_s + "%",
                 (get_pct_change_52w_h(stock)*100).round(2).to_s.colorize(:red)+ "%".colorize(:red), 
                 (get_pct_change_52w_l(stock)*100).round(2).to_s.colorize(:green)+ "%".colorize(:red)])
     end
 
     table = Terminal::Table.new :rows => rows
-    table = Terminal::Table.new :headings => ['Stock', 'Price','% From Prev Day','% From 52 Week High','% From 52 Week Low'], :rows => rows
+    table = Terminal::Table.new :headings => ['Stock', 'Price','% Change Prev Day','% Change YTD','% From 52 Week High','% From 52 Week Low'], :rows => rows
     puts table
 end
 
 # Displays extended information compared to print_single_stock_price
-# Price, % change from yesterday, % change ytd, % change from 52w_high, % change from 52w_low 
+# Price, price change from yesterday, price change ytd, price change from 52w_high, price change from 52w_low 
 def print_single_stock_price(stock)
+    rows = []
+    if get_price_change_y_day(stock) > 0 && get_price_change_ytd(stock) > 0
+        rows.push([get_stock_symbol(stock), "$".colorize(:green) + get_stock_price(stock).to_s.colorize(:green),
+            "$".colorize(:green) + get_price_change_y_day(stock).to_s.colorize(:green),
+            "$".colorize(:green) + get_price_change_ytd(stock).to_s.colorize(:green),
+            "$".colorize(:red) + get_price_change_52w_h(stock).to_s.colorize(:red), 
+            "$".colorize(:green) + get_price_change_52w_l(stock).to_s.colorize(:green)])
+    elsif get_price_change_y_day(stock) > 0 && get_price_change_ytd(stock) < 0
+        rows.push([get_stock_symbol(stock), "$".colorize(:green) + get_stock_price(stock).to_s.colorize(:green),
+            "$".colorize(:green) + get_price_change_y_day(stock).to_s.colorize(:green), 
+            "$".colorize(:red) + get_price_change_ytd(stock).to_s.colorize(:red),
+            "$".colorize(:red) + get_price_change_52w_h(stock).to_s.colorize(:red), 
+            "$".colorize(:green) + get_price_change_52w_l(stock).to_s.colorize(:green)])
+    elsif get_price_change_y_day(stock) < 0 && get_price_change_ytd(stock) > 0
+        rows.push([get_stock_symbol(stock), "$".colorize(:red) + get_stock_price(stock).to_s.colorize(:red),
+            "$".colorize(:red) + get_price_change_y_day(stock).to_s.colorize(:red), 
+            "$".colorize(:green) + get_price_change_ytd(stock).to_s.colorize(:green),
+            "$".colorize(:red) + get_price_change_52w_h(stock).to_s.colorize(:red), 
+            "$".colorize(:green) + get_price_change_52w_l(stock).to_s.colorize(:green)])
+    elsif get_price_change_y_day(stock) < 0 && get_price_change_ytd(stock) < 0
+        rows.push([get_stock_symbol(stock), "$".colorize(:red) + get_stock_price(stock).to_s.colorize(:red),
+            "$".colorize(:red) + get_price_change_y_day(stock).to_s.colorize(:red), 
+            "$".colorize(:red) + get_price_change_ytd(stock).to_s.colorize(:red),
+            "$".colorize(:red) + get_price_change_52w_h(stock).to_s.colorize(:red), 
+            "$".colorize(:green) + get_price_change_52w_l(stock).to_s.colorize(:green)])
+    end
 
+    table = Terminal::Table.new :rows => rows
+    table = Terminal::Table.new :headings => ['Stock', 'Price','Price Change from Prev Day','Price Change YTD','Change from 52 Week High','Change From 52 Week Low'], :rows => rows
+    puts table
+end
 
+# User story: Martin wanted to be able to see a list of all S&P500 tickers
+def print_list_of_stock_tickers
+    list_of_keys = SP500_HASH_TABLE.keys()
+    
+    for i in 0..list_of_keys.length-1
+        puts list_of_keys[i]
+    end
 end
 
 puts "Welcome to the S&P 500 Stock Tracker"
 puts "You can check the prices of stocks that are in the S&P 500 by entering a stock ticker"
 
 while make_stock_price_check == true
+    # puts "This is a list of all the S&P 500 stock tickers"
+    # print_list_of_stock_tickers()
     puts "Enter a stock ticker from the S&P500:"
     stock_ticker = gets.chomp.upcase
     stock = get_stock(stock_ticker)
     # print_single_stock_info(stock)
-    print_single_stock_price_pct(stock)
+    # print_single_stock_price_pct(stock)
+    print_single_stock_price(stock)
 
     puts "Would you like to check another stock? (y/n):"
     another_stock_check = gets.chomp
